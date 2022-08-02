bc0f245b7b3c7ba1b65e6c99de4883825f01f574
Susbot <susbot651968498@gmail.com>
2022-08-01 19:24:16 -0600

    Added API_requests

--- apis/views.py
+++ apis/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
 
+
 # Create your views here.
+# https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=84070&distance=50&API_KEY=1C27F481-552D-4922-BDE4-211DBC9A89C0
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=84070&distance=50&API_KEY=1C27F481-552D-4922-BDE4-211DBC9A89C0")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error....."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
