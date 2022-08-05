8680b84ddabc126d88cb454ca67cb826368d7776
Susbot <susbot651968498@gmail.com>
2022-08-04 18:27:28 -0600

    Bootstrap

--- apis/views.py
+++ apis/views.py
@@ -15,8 +15,33 @@ def home(request):
     except Exception as e:
         api = "Error....."
 
-    return render(request, 'home.html', {'api': api})
+    if api[0]['Category']['Name'] == "Good":
+        category_description = "(0 to 50) Air quality is satisfactory, and air pollution poses little or no risk."
+        category_color = "Good"
+    elif api[0]['Category']['Name'] == "Moderate":
+        category_description = "(51 to 100) Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution"
+        category_color = "Moderate"
+    elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+        category_description = "(101 to 150) Members of sensitive groups may experience health effects. The general public is less likely to be affected."
+        category_color = "Usg"
+    elif api[0]['Category']['Name'] == "Unhealthy":
+        category_description = "(151 to 200) Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects"
+        category_color = "Unhealthy"
+    elif api[0]['Category']['Name'] == "Very Unhealthy":
+        category_description = "(201 to 300) Health alert: The risk of health effects is increased for everyone."
+        category_color = "Veryunhealthy"
+    elif api[0]['Category']['Name'] == "Hazardous":
+        category_description = " (301 and higher) Health warning of emergency conditions: everyone is more likely to be affected."
+        category_color = "Hazardous"
+
+    return render(request, 'home.html', {
+                'api': api,
+                'category_description': category_description,
+                'category_color': category_color})
 
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+
