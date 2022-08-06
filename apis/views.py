71bce01ad76b30c17dbaedb5c3c9468959c4d721
Susbot <susbot651968498@gmail.com>
2022-08-05 18:11:13 -0600

    Weather App

--- apis/views.py
+++ apis/views.py
@@ -1,47 +1,80 @@
 from django.shortcuts import render
 
 
-
 # Create your views here.
 # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=84070&distance=50&API_KEY=1C27F481-552D-4922-BDE4-211DBC9A89C0
 def home(request):
     import json
     import requests
 
-    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=84070&distance=50&API_KEY=1C27F481-552D-4922-BDE4-211DBC9A89C0")
+    if request.method == "POST":
+        zipcode = request.POST['zipcode']
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json"
+                                   "&zipCode=" + zipcode + '&distance=50&API_KEY=1C27F481-552D-4922-BDE4-211DBC9A89C0')
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error....."
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error....."
+        if api[0]['Category']['Name'] == "Good":
+            category_description = "(0 to 50) Air quality is satisfactory, and air pollution poses little or no risk."
+            category_color = "Good"
+        elif api[0]['Category']['Name'] == "Moderate":
+            category_description = "(51 to 100) Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution"
+            category_color = "Moderate"
+        elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+            category_description = "(101 to 150) Members of sensitive groups may experience health effects. The general public is less likely to be affected."
+            category_color = "Usg"
+        elif api[0]['Category']['Name'] == "Unhealthy":
+            category_description = "(151 to 200) Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects"
+            category_color = "Unhealthy"
+        elif api[0]['Category']['Name'] == "Very Unhealthy":
+            category_description = "(201 to 300) Health alert: The risk of health effects is increased for everyone."
+            category_color = "Veryunhealthy"
+        elif api[0]['Category']['Name'] == "Hazardous":
+            category_description = " (301 and higher) Health warning of emergency conditions: everyone is more likely to be affected."
+            category_color = "Hazardous"
 
-    if api[0]['Category']['Name'] == "Good":
-        category_description = "(0 to 50) Air quality is satisfactory, and air pollution poses little or no risk."
-        category_color = "Good"
-    elif api[0]['Category']['Name'] == "Moderate":
-        category_description = "(51 to 100) Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution"
-        category_color = "Moderate"
-    elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
-        category_description = "(101 to 150) Members of sensitive groups may experience health effects. The general public is less likely to be affected."
-        category_color = "Usg"
-    elif api[0]['Category']['Name'] == "Unhealthy":
-        category_description = "(151 to 200) Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects"
-        category_color = "Unhealthy"
-    elif api[0]['Category']['Name'] == "Very Unhealthy":
-        category_description = "(201 to 300) Health alert: The risk of health effects is increased for everyone."
-        category_color = "Veryunhealthy"
-    elif api[0]['Category']['Name'] == "Hazardous":
-        category_description = " (301 and higher) Health warning of emergency conditions: everyone is more likely to be affected."
-        category_color = "Hazardous"
+        return render(request, 'home.html', {
+            'api': api,
+            'category_description': category_description,
+            'category_color': category_color})
 
-    return render(request, 'home.html', {
-                'api': api,
-                'category_description': category_description,
-                'category_color': category_color})
+
+    else:
+
+        api_request = requests.get(
+            "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=84070&distance=50&API_KEY=1C27F481-552D-4922-BDE4-211DBC9A89C0")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error....."
+
+        if api[0]['Category']['Name'] == "Good":
+            category_description = "(0 to 50) Air quality is satisfactory, and air pollution poses little or no risk."
+            category_color = "Good"
+        elif api[0]['Category']['Name'] == "Moderate":
+            category_description = "(51 to 100) Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution"
+            category_color = "Moderate"
+        elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+            category_description = "(101 to 150) Members of sensitive groups may experience health effects. The general public is less likely to be affected."
+            category_color = "Usg"
+        elif api[0]['Category']['Name'] == "Unhealthy":
+            category_description = "(151 to 200) Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects"
+            category_color = "Unhealthy"
+        elif api[0]['Category']['Name'] == "Very Unhealthy":
+            category_description = "(201 to 300) Health alert: The risk of health effects is increased for everyone."
+            category_color = "Veryunhealthy"
+        elif api[0]['Category']['Name'] == "Hazardous":
+            category_description = " (301 and higher) Health warning of emergency conditions: everyone is more likely to be affected."
+            category_color = "Hazardous"
+
+        return render(request, 'home.html', {
+            'api': api,
+            'category_description': category_description,
+            'category_color': category_color})
 
 
 def about(request):
     return render(request, 'about.html', {})
-
-
-
